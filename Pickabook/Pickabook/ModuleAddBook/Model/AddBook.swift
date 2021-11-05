0974164ee6c122d7a42437390f25f83e28fe562a
Timur <itimur317@gmail.com>
2021-11-05 04:13:04 +0300

    started

---
+++ Pickabook/Pickabook/ModuleAddBook/Model/AddBook.swift
@@ -0,0 +1,15 @@
+//
+//  AddBook.swift
+//  Pickabook
+//
+//  Created by Timur on 05.11.2021.
+//
+
+import Foundation
+
+struct Book {
+    var nameBook: String
+ //   var nameAuthor: String
+   // var condition: Int
+    
+}
