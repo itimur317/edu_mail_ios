3940e631458e13b85d4555d9680fe97e3ce2a3fd
Dannoster <daniilnaiko@mail.ru>
2021-12-17 15:03:23 +0300

    tried make reloading db, look to replace databasedata at changeprofiledata

--- Pickabook/Pickabook/Profiles/ChangeProfileData/ChangeProfileDataPresenter.swift
+++ Pickabook/Pickabook/Profiles/ChangeProfileData/ChangeProfileDataPresenter.swift
@@ -8,10 +8,12 @@
 import Foundation
 import FirebaseFirestore
 import FirebaseAuth
+import FirebaseDatabase
 //output
 protocol ChangeProfileDataViewControllerProtocol: AnyObject {
     func openSavedPhotosAlbum()
     func reloadMyProfile(myProfile: Profile)
+    func didTapSaveButton()
     //func presentProfile(profiles: [Profile])
     //func presentAlert(title: String, message: String)
     //var bookList: [Book]? { get }
@@ -27,7 +29,7 @@ protocol ChangeProfileDataPresenterProtocol: AnyObject {
     func observeMyProfile()
     
     func didTapAddPhotoButton()
-    func didTapAddButton()
+    func replaceDatabaseData(with newData : Profile)
     func setViewDelegate(delegate: ChangeProfileDataViewControllerProtocol)
 }
  
@@ -53,8 +55,107 @@ final class ChangeProfileDataPresenter: ChangeProfileDataPresenterProtocol {
         self.view?.openSavedPhotosAlbum()
     }
  
-    func didTapAddButton() {
-        //
+    func replaceDatabaseData(with newData: Profile) {
+        
+//        guard let MyId = Auth.auth().currentUser?.uid else { return }
+//        var key : String = ""
+//        
+//        let ref = Database.database().reference().root.child("Users").observe(DataEventType.value, with: { (snapshot) in
+//                
+//                if let snapshot = snapshot.children.allObjects as? [DataSnapshot] {
+//                    print("SNAPSHOT: \(snapshot)")
+//                    for snap in snapshot {
+//                        if let userDict = snap.value as? Dictionary<String, AnyObject> {
+//                            if userDict["id"] as? String == MyId {
+//                                key = snap.key
+//                                print(key)
+//                                //Add this key to userID array
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//        )
+//                                                                                            
+//        var ref2 = Database
+//            .database()
+//            .reference()
+//            .root
+//            .child("Users")
+//            .child(key)
+//            .updateChildValues(["name" : newData.name,
+//                                "phoneNumber" : newData.phoneNumber ?? nil,
+//                                "email" : newData.email,
+//                                "telegramLink" : newData.telegramLink ?? nil,
+//                                "instagramLink" : newData.instagramLink ?? nil])
+            
+                                                                       
+                                    
+                                        
+//            .queryOrdered(byChild: "id").queryEqual(toValue: MyId)
+//
+//        let UserRealId = ref.push().getKey();
+//
+//        ref.observe(.value, with: DataSnapshot) in {
+//
+//        }
+//
+//        ref = Database.parent
+//            .updateChildValues(["name" : newData.name,
+//                                "phoneNumber" : newData.phoneNumber ?? nil,
+//                                "email" : newData.email,
+//                                "telegramLink" : newData.telegramLink ?? nil,
+//                                "instagramLink" : newData.instagramLink ?? nil])
+//
+//        ref .child('users')
+//            .orderByChild('name')
+//            .equalTo('John Doe')
+//            .on("value", function(snapshot)
+//
+//
+//
+//        if newData.photo != nil {
+//
+//            let imageLoader: ImageLoaderProtocol = ImageLoader()
+//            guard let data = newData.photo?.jpegData(compressionQuality: 0.1) else { return }
+//
+//            imageLoader.uploadImage(imageData: [data]) { [weak self] imageURLs, imageNames in
+//
+                
+    //            photoName = imageNames
+    //            photoURL = imageURLs
+                
+    //            var dictForDatabase : [String : Any]
+    //            dictForDatabase = ["id" : MyId,
+    //                               "name" : newData.name,
+    //                               "phoneNumber" : newData.phoneNumber ?? nil,
+    //                               "email" : newData.email,
+    //                               "telegramLink" : newData.telegramLink ?? nil,
+    //                               "instagramLink" : newData.instagramLink ?? nil,
+    //                               "photoName" : imageNames,
+    //                               "photoURL" : imageURLs]
+                
+//                dataSnapshot.getKey()
+//                //let uid = FirebaseAuth. getInstance().getCurrentUser().getUid()
+//                let ref = Database
+//                    .database()
+//                    .reference()
+//                    .root
+//                    .child("Users")
+//                    .child(uid)
+//                    .updateChildValues(["photoName" : imageNames,
+//                                        "photoURL" : imageURLs])
+//
+//                self?.database.collection("Users").addDocument(data: dictForDatabase) { [weak self] error in
+//                    if let error = error { print("Error writing document: \(error)") }
+//                    else {
+//                        print("Document successfully written!")
+//                        //self?.output?.didCreate(user)
+//                    }
+//                }
+//
+//            }
+//        }
     }
  
 }
